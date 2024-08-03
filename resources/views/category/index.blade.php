7734d9f2b968a895611d603b5c9d9424137975c5
Zac <a19860101@gmail.com>
2024-08-03 14:04:22 +0800

    20240803

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -13,20 +13,10 @@
     </style>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <form action="{{route('category.store')}}" method="post">
-                    @csrf
-                    <div>
-                        <label for="">分類標題</label>
-                        <input type="text" name="title">
-                    </div>
-                    <div>
-                        <label for="">分類英文標題</label>
-                        <input type="text" name="slug">
-                    </div>
-                    <input type="submit" value="新增分類">
-                </form>
-                <table>
+           
+            <div class="col-9">
+                <h3>所有分類</h3>
+                <table class="table">
                     <tr>
                         <th>分類標題</th>
                         <th>分類英文標題</th>
@@ -54,6 +44,21 @@
                     </tr>
                     @endforeach
                 </table>
+            </div>
+            <div class="col-3">
+                <h3>新增分類</h3>
+                <form action="{{route('category.store')}}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="" class="form-label">分類標題</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">分類英文標題</label>
+                        <input type="text" name="slug" class="form-control">
+                    </div>
+                    <input type="submit" value="新增分類" class="btn btn-primary">
+                </form>
             </div>
         </div>
     </div>
