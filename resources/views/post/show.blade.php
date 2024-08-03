d8d252ecdd3398f52d16baf913a6be45d22195d1
Zac <a19860101@gmail.com>
2024-08-03 14:18:29 +0800

    20240803

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>{{$post->title}}</h2>
-    <div>建立時間:{{$post->created_at}}</div>
-    <div>
-        作者：{{$post->user->name}}
+<x-post-layout>
+    <x-slot:title>{{$post->title}}</x-slot:title>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 border border-dark p-5 rounded">
+                <h2>{{ $post->title }}</h2>
+                <hr>
+                <div>建立時間:{{ $post->created_at }}</div>
+                <div>
+                    作者：{{ $post->user->name }}
+                </div>
+                <div class="mb-3">
+                    {{ $post->body }}
+                </div>
+                <hr>
+                <div class="d-flex gap-3">
+                    <a href="{{ route('post.index') }}" class="btn btn-info btn-sm">文章列表</a>
+                    @auth
+                    @if (Auth::id() == $post->user->id)
+                    <form action="{{ route('post.delete', $post->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="刪除" onclick="return confirm('確認刪除？')" class="btn btn-danger btn-sm">
+                    </form>
+                    <a href="{{ route('post.edit', $post->id) }}" class="btn btn-success btn-sm">編輯文章</a>
+                    @endif
+                    @endauth
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-        {{$post->body}}
-    </div>
-    <a href="{{route('post.index')}}">文章列表</a>
-    @auth
-    @if(Auth::id() == $post->user->id)
-    <form action="{{route('post.delete',$post->id)}}" method="post">
-        @csrf
-        @method('delete')
-        <input type="submit" value="刪除" onclick="return confirm('確認刪除？')">
-    </form>
-    <a href="{{route('post.edit',$post->id)}}">編輯文章</a>
-    @endif
-    @endauth
-</body>
-</html>
+</x-post-layout>
